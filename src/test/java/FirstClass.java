73a2d79bd367fd9a0f5998f3867013f482f65e39
Mevlut Kama <mtkama02@gmail.com>
2022-12-10 22:34:37 +0300

    SELENIUM

---
+++ src/test/java/FirstClass.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstClass {
+    public static void main(String[] args) {
+
+        // 1.Set up Chrome Driver
+        System.setProperty("webdriver.chrome.driver","./src/resources/drivers/chromedriver.exe");
+
+        // 2.Create Chrome Driver
+        WebDriver driver = new ChromeDriver();
+
+        // 3.Now that we created webdriver, we can automate web application
+        driver.get("https://www.techproeducation.com");
+
+
+
+    }
+}
