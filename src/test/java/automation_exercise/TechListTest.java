72b48e3216646f5f8e07649ba405e7ea34e2de7a
Mevlut Kama <mtkama02@gmail.com>
2023-02-11 21:55:26 +0300

    junit

--- src/test/java/automation_exercise/TechListTest.java
+++ src/test/java/automation_exercise/TechListTest.java
@@ -2,9 +2,7 @@ package automation_exercise;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import utilities.TestBase;
 
