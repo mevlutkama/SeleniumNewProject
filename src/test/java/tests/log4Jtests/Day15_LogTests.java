deb38f93724d8ece52a50b280202ffc1f1a5287d
Mevlut Kama <mtkama02@gmail.com>
2023-03-20 09:18:15 +0300

    junit

--- src/test/java/tests/log4Jtests/Day15_LogTests.java
+++ src/test/java/tests/log4Jtests/Day15_LogTests.java
@@ -3,7 +3,6 @@ package tests.log4Jtests;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
-
 public class Day15_LogTests {
     private static Logger logger = LogManager.getLogger(Day15_LogTests.class.getName());
     @Test
